6d2ab3d67744f13791cbe0509cd7a266bc03573c
Azarudeen <moaz@softwareag.com>
2019-11-05 18:47:22 +0530

    security scheme rule

--- src/main/java/com/softwareag/apilyzer/api/RuleEnum.java
+++ src/main/java/com/softwareag/apilyzer/api/RuleEnum.java
@@ -2,7 +2,9 @@ package com.softwareag.apilyzer.api;
 
 public enum RuleEnum {
   MISSING_INFO_DESC("MISSING_INFO_DESC", "Description is missing in the Info section", "Description is missing in the Info section", "To fix this issue, please give a value to the  description field in the Info section"),
-  MISSING_SERVER_DESC("MISSING_SERVER_DESC", "Description is missing in the Server section", "Description is missing in the Server section", "To fix this issue, please give a value to the  description field in the Server section");
+  MISSING_SERVER_DESC("MISSING_SERVER_DESC", "Description is missing in the Server section", "Description is missing in the Server section", "To fix this issue, please give a value to the  description field in the Server section"),
+  SECURITY_SCHEME("HTTP_SECURITY_SCHEME","API accepts HTTP requests in the clear","The API accepts HTTP communications in the clear. HTTP traffic is not encrypted and can thus be easily intercepted.","Remove http from the schemes list, and only include https");
+
   private String rulename, summary, description, remedy;
 
 
--- src/main/java/com/softwareag/apilyzer/engine/rules/SecuritySchemeRule.java
+++ src/main/java/com/softwareag/apilyzer/engine/rules/SecuritySchemeRule.java
@@ -1,18 +1,23 @@
 package com.softwareag.apilyzer.engine.rules;
 
-import com.softwareag.apilyzer.api.CategoryEnum;
-import com.softwareag.apilyzer.api.IRuleSpecification;
-import com.softwareag.apilyzer.api.SeverityEnum;
-import com.softwareag.apilyzer.api.SubCategoryEnum;
+import com.softwareag.apilyzer.api.*;
 import com.softwareag.apilyzer.model.Issue;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class SecuritySchemeRule implements IRuleSpecification {
+  List<Issue> issues = Collections.emptyList();
+
   @Override
   public String getRuleName() {
-    return null;
+    return RuleEnum.SECURITY_SCHEME.getRulename();
   }
 
   @Override
@@ -32,17 +37,17 @@ public class SecuritySchemeRule implements IRuleSpecification {
 
   @Override
   public String getSummary() {
-    return null;
+    return RuleEnum.SECURITY_SCHEME.getSummary();
   }
 
   @Override
   public String getDescription() {
-    return null;
+    return RuleEnum.SECURITY_SCHEME.getDescription();
   }
 
   @Override
   public String getRemedy() {
-    return null;
+    return RuleEnum.SECURITY_SCHEME.getRemedy();
   }
 
   @Override
@@ -52,6 +57,34 @@ public class SecuritySchemeRule implements IRuleSpecification {
 
   @Override
   public void execute(OpenAPI api) {
+    List<Server> servers = api.getServers();
+    List<Server> httpServers = servers.stream().filter(server -> server.getUrl().split(":")[0].equals("http")).collect(Collectors.toList());
+    for (Server server : httpServers) {
+      issues.add(createIssue(buildContext(server)));
+    }
+  }
+
+  private Function<String, String> getScheme = (url) -> {
+    return url.split(":")[0];
+  };
+
+  private Map<String, String> buildContext(Server server) {
+    Map<String, String> context = new HashMap<>();
+    context.put("rulename", RuleEnum.SECURITY_SCHEME.name());
+    context.put("rulepath", server.getUrl());
+    return context;
+  }
+
+  protected Issue createIssue(Map<String, String> context) {
+    Issue issue = new Issue();
+    issue.setDescription(getDescription());
+    issue.setErrorInfo(getErrorInfo());
+    issue.setName(getRuleName());
+    issue.setRemedy(getRemedy());
+    issue.setSeverity(getSeverity().name());
+    issue.setSummary(getSummary());
+    issue.setContext(context);
+    return issue;
   }
 
   @Override
