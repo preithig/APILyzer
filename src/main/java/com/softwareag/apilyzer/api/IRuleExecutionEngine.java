69dce2566fa00d90037eebf4bec08e12be12c574
nas <nagarajan.sivaraman@softwareag.com>
2019-11-05 12:56:16 +0530

    implementation class for rule execution engine

--- src/main/java/com/softwareag/apilyzer/api/IRuleExecutionEngine.java
+++ src/main/java/com/softwareag/apilyzer/api/IRuleExecutionEngine.java
@@ -1,6 +1,13 @@
 package com.softwareag.apilyzer.api;
 
+import com.softwareag.apilyzer.model.EvalutionResult;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import java.util.List;
+
 public interface IRuleExecutionEngine {
 
+    List<IRuleSpecification> getAllRules();
 
+    EvalutionResult evaluate(OpenAPI openAPI);
 }
--- src/main/java/com/softwareag/apilyzer/engine/RuleExecutionEngine.java
+++ src/main/java/com/softwareag/apilyzer/engine/RuleExecutionEngine.java
@@ -1,7 +1,22 @@
 package com.softwareag.apilyzer.engine;
 
 import com.softwareag.apilyzer.api.IRuleExecutionEngine;
+import com.softwareag.apilyzer.api.IRuleSpecification;
+import com.softwareag.apilyzer.model.EvalutionResult;
+import io.swagger.v3.oas.models.OpenAPI;
+
+import java.util.Collections;
+import java.util.List;
 
 public class RuleExecutionEngine implements IRuleExecutionEngine {
 
+    @Override
+    public List<IRuleSpecification> getAllRules() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public EvalutionResult evaluate(OpenAPI openAPI) {
+        return null;
+    }
 }
