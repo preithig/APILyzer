b52fd1030683b52ffe9ec2d37b4222d112618b94
Preethi G <preethig82@gmail.com>
2019-11-07 15:03:11 +0530

    evaluation date for the result

--- src/main/java/com/softwareag/apilyzer/model/EvaluationResult.java
+++ src/main/java/com/softwareag/apilyzer/model/EvaluationResult.java
@@ -7,6 +7,7 @@ import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Document(type = "default", indexName = "evaluations")
@@ -28,6 +29,25 @@ public class EvaluationResult {
 
   @Field(type = FieldType.Nested)
   private List<Category> categories = new ArrayList<>();
+
+  @Field(type = FieldType.Date)
+  private Date evaluationDate;
+
+  public String getApiId() {
+    return apiId;
+  }
+
+  public void setApiId(String apiId) {
+    this.apiId = apiId;
+  }
+
+  public Date getEvaluationDate() {
+    return evaluationDate;
+  }
+
+  public void setEvaluationDate(Date evaluationDate) {
+    this.evaluationDate = evaluationDate;
+  }
 
   public EvaluationResult() {
   }
