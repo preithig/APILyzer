85289933ebcbe9951cfb105133df8cf0b6d666db
Preethi G <preethig82@gmail.com>
2019-11-13 11:35:48 +0530

    Fix the example changes

--- src/main/java/com/softwareag/apilyzer/engine/fixes/MissingResponseExampleFix.java
+++ src/main/java/com/softwareag/apilyzer/engine/fixes/MissingResponseExampleFix.java
@@ -1,11 +1,12 @@
 package com.softwareag.apilyzer.engine.fixes;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.softwareag.apilyzer.api.IRuleFix;
 import com.softwareag.apilyzer.model.FixData;
 import com.softwareag.apilyzer.model.Issue;
+import io.apptik.json.JsonElement;
+import io.apptik.json.generator.JsonGenerator;
+import io.apptik.json.schema.SchemaV4;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
@@ -13,7 +14,10 @@ import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 
-import java.util.*;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class MissingResponseExampleFix implements IRuleFix {
 
@@ -25,32 +29,44 @@ public class MissingResponseExampleFix implements IRuleFix {
     Optional<Map.Entry<String, Operation>> operationOptional = operations.stream().filter(op -> op.getKey().equals(context.get("operationId"))).findAny();
     ApiResponse apiResponse = operationOptional.get().getValue().getResponses().get(context.get("responseKey"));
     MediaType mediaType = apiResponse.getContent().get(context.get("contentKey"));
+    try {
+      if (context.get("contentKey").equals("application/json")) {
 
-    if (context.get("contentKey").equals("application/json")) {
+        Schema schema = mediaType.getSchema();
+        //   String schemaType = schema.getType();
+        //    Schema s = stringSchemaMap.get(key);
 
-      Schema schema = mediaType.getSchema();
-      String schemaType = schema.getType();
+        io.apptik.json.schema.Schema ss = new SchemaV4().wrap(JsonElement.readFrom(new ObjectMapper().writeValueAsString(schema)).asJsonObject());
+        JsonElement elem = new JsonGenerator(ss, null).generate();
+        mediaType.setExample(elem.toString());
 
-      if (schemaType.equalsIgnoreCase("string")) {
+     /*   if (schemaType.equalsIgnoreCase("string")) {
 
-      } else {
-        Map<String, Schema> stringSchemaMap = schema.getProperties();
-        Set<String> keys = stringSchemaMap.keySet();
-        JsonNode jsonNode = new ObjectMapper().createObjectNode();
-        for (String key : keys) {
-          Schema s = stringSchemaMap.get(key);
+        } else {
+          Map<String, Schema> stringSchemaMap = schema.getProperties();
+          Set<String> keys = stringSchemaMap.keySet();
+          JsonNode jsonNode = new ObjectMapper().createObjectNode();
+          for (String key : keys) {
+            Schema s = stringSchemaMap.get(key);
 
-          if (s.getType().equalsIgnoreCase("string") &&
-              "([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})".equals(s.getPattern())) {
-            ((ObjectNode) jsonNode).put(key, UUID.randomUUID().toString());
-          } else if (s.getType().equalsIgnoreCase("Integer") && s.getMinimum() != null
-              && s.getMaximum() != null) {
-            ((ObjectNode) jsonNode).put(key, (int) (Math.random() * (s.getMaximum().intValue() - s.getMinimum().intValue())) + s.getMinimum().intValue());
+            // io.apptik.json.schema.Schema ss  = new SchemaV4().wrap(JsonElement.readFrom(new ObjectMapper().writeValueAsString(s)).asJsonObject());
+            //  io.apptik.json.schema.Schema ss = new SchemaV4().wrap(JsonObject.readFrom(s.toString()));
+            //     new JsonGenerator(ss, null).generate().toString();
+
+            if (s.getType().equalsIgnoreCase("string") &&
+                "([0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12})".equals(s.getPattern())) {
+              ((ObjectNode) jsonNode).put(key, UUID.randomUUID().toString());
+            } else if (s.getType().equalsIgnoreCase("Integer") && s.getMinimum() != null
+                && s.getMaximum() != null) {
+              ((ObjectNode) jsonNode).put(key, (int) (Math.random() * (s.getMaximum().intValue() - s.getMinimum().intValue())) + s.getMinimum().intValue());
+            }
           }
-        }
 
-        mediaType.setExample(jsonNode.toPrettyString());
+          mediaType.setExample(jsonNode.toPrettyString());
+        }*/
       }
+    } catch (IOException e) {
+
     }
     return openAPI;
   }
