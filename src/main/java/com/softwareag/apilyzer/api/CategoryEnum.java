b523f7fe972d2c096020d54a9f84f08a1f0a6b1a
Preethi G <preethig82@gmail.com>
2019-11-14 10:34:01 +0530

    Category enum change

--- src/main/java/com/softwareag/apilyzer/api/CategoryEnum.java
+++ src/main/java/com/softwareag/apilyzer/api/CategoryEnum.java
@@ -2,9 +2,29 @@ package com.softwareag.apilyzer.api;
 
 public enum CategoryEnum {
 
-  API_STANDARDS,
-  EASE_OF_USE,
-  SECURITY_STANDARDS,
+  API_STANDARDS("API Standards"),
+  EASE_OF_USE("Ease of Use"),
+  SECURITY_STANDARDS("Security Standards");
   //PERFORMANCE_STANDARDS;
+
+  private String categoryName;
+
+
+  CategoryEnum(String categoryName) {
+    this.categoryName = categoryName;
+  }
+
+  public String getCategoryName() {
+    return categoryName;
+  }
+
+  public static CategoryEnum get(String categoryName) {
+    for (CategoryEnum categoryEnum : CategoryEnum.values()) {
+      if (categoryEnum.getCategoryName().equals(categoryName)) {
+        return categoryEnum;
+      }
+    }
+    return null;
+  }
 
 }
